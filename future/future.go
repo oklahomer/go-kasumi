3db87696aa9cb6309142564d8bc47168321530b2
Oklahomer <hagiwara.go@gmail.com>
2022-02-03 21:20:45 +0900

    Only expose interface

--- future/future.go
+++ future/future.go
@@ -11,13 +11,63 @@ import (
 var ErrTimeout = errors.New("task execution timed out")
 
 // Future provides access to an ongoing event that is not finished, yet.
-type Future struct {
-	result chan *Result
+type Future interface {
+	// Block waits until the given function finishes and returns the Result.
+	Block() Result
+
+	// Wait waits until its execution finishes or the given ctx is canceled, and then returns the Result.
+	// A typical usage is somewhat like below:
+	//
+	//	// Initiate a time-consuming task
+	//	f := future.New(func() (interface{}, error)) {
+	// 		// Do some time-consuming task
+	//		return struct{}{}, nil
+	// 	}
+	//
+	//	// Prepare a context.Context that is canceled when the parentCtx is canceled or the timeout interval passes.
+	//	ctx, _ := context.WithTimeout(parentCtx, 3*time.Second)
+	//
+	//	// Wait til the task execution ends or the ctx is canceled.
+	//	result := f.Wait(ctx)
+	//
+	// When the given ctx is canceled before the execution completion, the returning Result always contains an error value of ErrTimeout.
+	Wait(ctx context.Context) Result
+
+	// C returns a channel that passes the Result of execution.
+	// Reading this channel without cancellation mechanism is equivalent to the call to Block;
+	// The call to this with a select statement with one alternative case of context.Done or timer.C is equivalent to Wait.
+	// This method is beneficial when a more flexible select statement with multiple alternative cases is in use.
+	//
+	//	for {
+	//		select {
+	//		case result := <- future1.C():
+	//			return result, nil
+	//
+	//		case result := <- future2.C():
+	//			return result, nil
+	//
+	//		case <- ticker.C:
+	//			fmt.Println(`waiting...`)
+	//
+	//		case <- ctx.Done():
+	//			return nil, errors.New(`timed out`)
+	//		}
+	//	}
+	C() <-chan Result
+
+	// Then lets a given function to be executed after the preceding task.
+	// When the preceding task finishes successfully, then the result value which is equivalent to Result.Value is passed to fn;
+	// When the preceding task finishes with an error, then this immediately returns a Future with the preceding error.
+	Then(fn func(value interface{}) (interface{}, error)) Future
+}
+
+type future struct {
+	result chan Result
 }
 
 // New creates a Future instance with the given executable function.
 // Subscribing to this instance gives asynchronous access to the result.
-func New(fn func() (interface{}, error)) *Future {
+func New(fn func() (interface{}, error)) Future {
 	f := newFuture()
 
 	go func() {
@@ -29,32 +79,32 @@ func New(fn func() (interface{}, error)) *Future {
 
 // NewError creates a Future instance that returns the given error.
 // Since this does not dispatch a task to a new goroutine, this is more efficient than calling New() with a function that returns an error.
-func NewError(err error) *Future {
+func NewError(err error) Future {
 	f := newFuture()
-	f.result <- &Result{Error: err}
+	f.result <- Result{Error: err}
 
 	return f
 }
 
-func newFuture() *Future {
-	return &Future{
-		result: make(chan *Result, 1),
+func newFuture() *future {
+	return &future{
+		result: make(chan Result, 1),
 	}
 }
 
-func execute(fn func() (interface{}, error)) (result *Result) {
+func execute(fn func() (interface{}, error)) (result Result) {
 	defer func() {
 		r := recover()
 		if r != nil {
 			switch typed := r.(type) {
 			case error:
 				err := fmt.Errorf("panic on Future: %w", typed)
-				result = &Result{
+				result = Result{
 					Error: err,
 				}
 
 			default:
-				result = &Result{
+				result = Result{
 					Error: fmt.Errorf("panic on Future: %+v", typed),
 				}
 			}
@@ -62,73 +112,33 @@ func execute(fn func() (interface{}, error)) (result *Result) {
 	}()
 
 	r, err := fn()
-	return &Result{
+	return Result{
 		Value: r,
 		Error: err,
 	}
 }
 
-// Block waits until the given function finishes and returns the Result.
-func (f *Future) Block() *Result {
+func (f *future) Block() Result {
 	return <-f.result
 }
 
-// Wait waits until its execution finishes or the given ctx is canceled, and then returns the Result.
-// A typical usage is somewhat like below:
-//
-//	// Initiate a time-consuming task
-//	f := future.New(func() (interface{}, error)) {
-// 		// Do some time-consuming task
-//		return struct{}{}, nil
-// 	}
-//
-//	// Prepare a context.Context that is canceled when the parentCtx is canceled or the timeout interval passes.
-//	ctx, _ := context.WithTimeout(parentCtx, 3*time.Second)
-//
-//	// Wait til the task execution ends or the ctx is canceled.
-//	result := f.Wait(ctx)
-//
-// When the given ctx is canceled before the execution completion, the returning Result always contains an error value of ErrTimeout.
-func (f *Future) Wait(ctx context.Context) *Result {
+func (f *future) Wait(ctx context.Context) Result {
 	select {
 	case result := <-f.result:
 		return result
 
 	case <-ctx.Done():
-		return &Result{
+		return Result{
 			Error: ErrTimeout,
 		}
 	}
 }
 
-// C returns a channel that passes the Result of execution.
-// Reading this channel without cancellation mechanism is equivalent to the call to Block;
-// The call to this with a select statement with one alternative case of context.Done or timer.C is equivalent to Wait.
-// This method is beneficial when a more flexible select statement with multiple alternative cases is in use.
-//
-//	for {
-//		select {
-//		case result := <- future1.C():
-//			return result, nil
-//
-//		case result := <- future2.C():
-//			return result, nil
-//
-//		case <- ticker.C:
-//			fmt.Println(`waiting...`)
-//
-//		case <- ctx.Done():
-//			return nil, errors.New(`timed out`)
-//		}
-//	}
-func (f *Future) C() <-chan *Result {
+func (f *future) C() <-chan Result {
 	return f.result
 }
 
-// Then lets a given function to be executed after the preceding task.
-// When the preceding task finishes successfully, then the result value which is equivalent to Result.Value is passed to fn;
-// When the preceding task finishes with an error, then this immediately returns a Future with the preceding error.
-func (f *Future) Then(fn func(value interface{}) (interface{}, error)) *Future {
+func (f *future) Then(fn func(value interface{}) (interface{}, error)) Future {
 	return New(func() (interface{}, error) {
 		// Wait till the initial task ends
 		result := f.Block()
